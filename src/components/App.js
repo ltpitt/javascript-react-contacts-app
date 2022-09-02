3ed7f5a09876384979b8b221516b5863378652b2
Davide Nastri <d.nastri@gmail.com>
2022-09-02 22:40:19 +0200

    feat: First commit

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import "../css/App.css";
+
+const contacts = [
+  {
+    id: "karen",
+    name: "Karen Isgrigg",
+    handle: "karen_isgrigg",
+    avatarURL: "http://localhost:5001/karen.jpg",
+  },
+  {
+    id: "richard",
+    name: "Richard Kalehoff",
+    handle: "richardkalehoff",
+    avatarURL: "http://localhost:5001/richard.jpg",
+  },
+  {
+    id: "tyler",
+    name: "Tyler McGinnis",
+    handle: "tylermcginnis",
+    avatarURL: "http://localhost:5001/tyler.jpg",
+  },
+];
+
+
+const App = () => {
+  return <div>Hello World</div>;
+};
+
+export default App;
