cb182cebda9a8f3aa62b2586cee1b5c490a27686
Davide Nastri <d.nastri@gmail.com>
2022-09-05 23:19:27 +0200

    feat: Added prop-types and implementing search query

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,38 @@
+import { useState } from "react";
 import "../css/App.css";
-
-const contacts = [
-  {
-    id: "karen",
-    name: "Karen Isgrigg",
-    handle: "karen_isgrigg",
-    avatarURL: "http://localhost:5001/karen.jpg",
-  },
-  {
-    id: "richard",
-    name: "Richard Kalehoff",
-    handle: "richardkalehoff",
-    avatarURL: "http://localhost:5001/richard.jpg",
-  },
-  {
-    id: "tyler",
-    name: "Tyler McGinnis",
-    handle: "tylermcginnis",
-    avatarURL: "http://localhost:5001/tyler.jpg",
-  },
-];
-
+import ListContacts from "./ListContacts";
 
 const App = () => {
-  return <div>Hello World</div>;
+  const removeContact = (contact) => {
+    setContacts(contacts.filter((c) => c.id !== contact.id));
+  };
+
+  const [contacts, setContacts] = useState([
+    {
+      id: "karen",
+      name: "Karen Isgrigg",
+      handle: "karen_isgrigg",
+      avatarURL: "http://localhost:5001/karen.jpg",
+    },
+    {
+      id: "richard",
+      name: "Richard Kalehoff",
+      handle: "richardkalehoff",
+      avatarURL: "http://localhost:5001/richard.jpg",
+    },
+    {
+      id: "tyler",
+      name: "Tyler McGinnis",
+      handle: "tylermcginnis",
+      avatarURL: "http://localhost:5001/tyler.jpg",
+    },
+  ]);
+
+  return (
+    <div>
+      <ListContacts onDeleteContact={removeContact} contacts={contacts} />
+    </div>
+  );
 };
 
 export default App;
--- src/components/ListContacts.js
+++ src/components/ListContacts.js
@@ -1,31 +1,52 @@
+import propTypes from "prop-types";
+import { useState } from "react";
+
 const ListContacts = ({ contacts, onDeleteContact }) => {
-  console.log(contacts);
+  const [query, setQuery] = useState("");
+  const updateQuery = (query) => {
+    setQuery(query.trim());
+  };
+
   return (
-    <ol className="contact-list">
-      {contacts.map((contact) => {
-        return (
-          <li key={contact.id} className="contact-list-item">
-            <div
-              className="contact-avatar"
-              style={{
-                backgroundImage: `url(${contact.avatarURL})`,
-              }}
-            ></div>
-            <div className="contact-details">
-              <p>{contact.name}</p>
-              <p>{contact.handle}</p>
-            </div>
-            <button
-              className="contact-remove"
-              onClick={() => onDeleteContact(contact)}
-            >
-              Remove
-            </button>
-          </li>
-        );
-      })}
-    </ol>
+    <div className="list-contacts">
+      <input
+        className="searchContacts"
+        type="text"
+        placeholder="Search Contacts"
+        value={query}
+        onChange={(event) => updateQuery(event.target.value)}
+      />
+      <ol className="contact-list">
+        {contacts.map((contact) => {
+          return (
+            <li key={contact.id} className="contact-list-item">
+              <div
+                className="contact-avatar"
+                style={{
+                  backgroundImage: `url(${contact.avatarURL})`,
+                }}
+              ></div>
+              <div className="contact-details">
+                <p>{contact.name}</p>
+                <p>{contact.handle}</p>
+              </div>
+              <button
+                className="contact-remove"
+                onClick={() => onDeleteContact(contact)}
+              >
+                Remove
+              </button>
+            </li>
+          );
+        })}
+      </ol>
+    </div>
   );
+};
+
+ListContacts.propTypes = {
+  contacts: propTypes.array.isRequired,
+  onDeleteContact: propTypes.func,
 };
 
 export default ListContacts;
