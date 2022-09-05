d3029cd86217fdaa76981abcb335c4d40b39d12e
Davide Nastri <d.nastri@gmail.com>
2022-09-05 22:29:56 +0200

    feat: Added ListContacts component

---
+++ src/components/ListContacts.js
@@ -0,0 +1,31 @@
+const ListContacts = ({ contacts, onDeleteContact }) => {
+  console.log(contacts);
+  return (
+    <ol className="contact-list">
+      {contacts.map((contact) => {
+        return (
+          <li key={contact.id} className="contact-list-item">
+            <div
+              className="contact-avatar"
+              style={{
+                backgroundImage: `url(${contact.avatarURL})`,
+              }}
+            ></div>
+            <div className="contact-details">
+              <p>{contact.name}</p>
+              <p>{contact.handle}</p>
+            </div>
+            <button
+              className="contact-remove"
+              onClick={() => onDeleteContact(contact)}
+            >
+              Remove
+            </button>
+          </li>
+        );
+      })}
+    </ol>
+  );
+};
+
+export default ListContacts;
