60e2de12a4c2834e3262f56970cc9dab6c1211ff
Davide Nastri <d.nastri@gmail.com>
2022-09-07 22:34:00 +0200

    feat: Implemented Contact search

--- src/components/ListContacts.js
+++ src/components/ListContacts.js
@@ -3,21 +3,43 @@ import { useState } from "react";
 
 const ListContacts = ({ contacts, onDeleteContact }) => {
   const [query, setQuery] = useState("");
+
   const updateQuery = (query) => {
     setQuery(query.trim());
   };
 
+  const clearQuery = () => {
+    setQuery("");
+  };
+
+  const showingContacts =
+    query === ""
+      ? contacts
+      : contacts.filter((c) =>
+          c.name.toLowerCase().includes(query.toLowerCase())
+        );
+
   return (
     <div className="list-contacts">
       <input
-        className="searchContacts"
+        className="search-contacts"
         type="text"
         placeholder="Search Contacts"
         value={query}
         onChange={(event) => updateQuery(event.target.value)}
       />
+
+      {showingContacts.length !== contacts.length && (
+        <div className="showing-contacts">
+          <span>
+            Now showing {showingContacts.length} of {contacts.length}
+          </span>
+          <button onClick={clearQuery}>Show all</button>
+        </div>
+      )}
+
       <ol className="contact-list">
-        {contacts.map((contact) => {
+        {showingContacts.map((contact) => {
           return (
             <li key={contact.id} className="contact-list-item">
               <div
