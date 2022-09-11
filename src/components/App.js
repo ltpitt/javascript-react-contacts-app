41a59ba5a9a99c52e73d6826da95ccf9a5f59fdc
Davide Nastri <d.nastri@gmail.com>
2022-09-11 11:33:03 +0200

    feat: Adding CreateConctact

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,60 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import "../css/App.css";
 import ListContacts from "./ListContacts";
+import CreateContact from "./CreateContact.js";
+import * as ContactsAPI from "../utils/ContactsAPI";
 
 const App = () => {
+  let navigate = useNavigate();
+
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    const getContacts = async () => {
+      const res = await ContactsAPI.getAll();
+      setContacts(res);
+    };
+
+    getContacts();
+  }, []);
+
   const removeContact = (contact) => {
+    ContactsAPI.remove(contact);
+
     setContacts(contacts.filter((c) => c.id !== contact.id));
   };
 
-  const [contacts, setContacts] = useState([
-    {
-      id: "karen",
-      name: "Karen Isgrigg",
-      handle: "karen_isgrigg",
-      avatarURL: "http://localhost:5001/karen.jpg",
-    },
-    {
-      id: "richard",
-      name: "Richard Kalehoff",
-      handle: "richardkalehoff",
-      avatarURL: "http://localhost:5001/richard.jpg",
-    },
-    {
-      id: "tyler",
-      name: "Tyler McGinnis",
-      handle: "tylermcginnis",
-      avatarURL: "http://localhost:5001/tyler.jpg",
-    },
-  ]);
+  const createContact = (contact) => {
+    const create = async () => {
+      const res = await ContactsAPI.create(contact);
+      setContacts(contacts.concat(res));
+    };
+
+    create();
+    navigate("/");
+  };
 
   return (
-    <div>
-      <ListContacts onDeleteContact={removeContact} contacts={contacts} />
-    </div>
+    <Routes>
+      <Route
+        exact
+        path="/"
+        element={
+          <ListContacts contacts={contacts} onDeleteContact={removeContact} />
+        }
+      />
+      <Route
+        path="/create"
+        element={
+          <CreateContact
+            onCreateContact={(contact) => {
+              createContact(contact);
+            }}
+          />
+        }
+      />
+    </Routes>
   );
 };
 
